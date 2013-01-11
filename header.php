a90147a0aad0af9a61580c2709fc66a89307e77f
Abdelmomen <abdelmomen1985@gmail.com>
2013-01-11 20:01:05 +0200

    add header

---
+++ header.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Aquatic Theme - Free CSS Template</title>
+<meta name="keywords" content="aquatic theme, aqua color, aquamarine, free template" />
+<meta name="description" content="Aquatic Theme is a free CSS template provided by templatemo.com" />
+<link href="css/templatemo_style.css" rel="stylesheet" type="text/css" />
+
+<link rel="stylesheet" type="text/css" href="css/ddsmoothmenu.css" />
+
+<link rel="stylesheet" type="text/css" href="rtl.css" />
+
+<script type="text/javascript" src="js/jquery.min.js"></script>
+<script type="text/javascript" src="js/ddsmoothmenu.js">
+
+/***********************************************
+* Smooth Navigational Menu- (c) Dynamic Drive DHTML code library (www.dynamicdrive.com)
+* This notice MUST stay intact for legal use
+* Visit Dynamic Drive at http://www.dynamicdrive.com/ for full source code
+***********************************************/
+
+</script>
+
+<script type="text/javascript">
+
+ddsmoothmenu.init({
+	mainmenuid: "templatemo_menu", //menu DIV id
+	orientation: 'h', //Horizontal or vertical menu: Set to "h" or "v"
+	classname: 'ddsmoothmenu', //class added to menu's outer DIV
+	//customtheme: ["#1c5a80", "#18374a"],
+	contentsource: "markup" //"markup" or ["container_id", "path_to_menu_file"]
+})
+
+</script>
+    
+</head>
+<body>
+
+<div id="templatemo_wrapper">
+
+	<div id="templatemo_header">
+    
+        <div id="site_title"><h1><a href="http://www.templatemo.com">Free CSS Templates</a></h1></div>
+        
+        <div id="templatemo_menu" class="ddsmoothmenu">
+            <ul>
+                <li><a href="index.html" class="selected">الرئيسية</a></li>
+                <li><a href="about.html">من نحن</a>
+                    <ul>
+                    	<li><a href="http://www.templatemo.com/page/1">محتوي عربي 1</a></li>
+                        <li><a href="http://www.templatemo.com/page/2">محتوي عربي 2</a></li>
+                        <li><a href="http://www.templatemo.com/page/3">محتوي عربي 3</a></li>
+                    </ul>
+                </li>
+                <li><a href="portfolio.html">اعمالنا</a>
+                    <ul>
+                    	<li><a href="http://www.templatemo.com/page/1">محتوي عربي 1</a></li>
+                        <li><a href="http://www.templatemo.com/page/2">محتوي عربي 2</a></li>
+                        <li><a href="http://www.templatemo.com/page/3">محتوي عربي 3</a></li>
+                    </ul>
+                </li>
+                <li><a href="services.html">خدمات</a>
+                    <ul>
+                    	<li><a href="http://www.templatemo.com/page/1">Sub menu 1</a></li>
+                        <li><a href="http://www.templatemo.com/page/2">Sub menu 2</a></li>
+                        <li><a href="http://www.templatemo.com/page/3">Sub menu 3</a></li>
+                        <li><a href="http://www.templatemo.com/page/4">Sub menu 4</a></li>
+                        <li><a href="http://www.templatemo.com/page/5">Sub menu 5</a></li>
+                    </ul>
+                </li>
+                <li><a href="contact.html">اتصل بنا</a></li>
+            </ul>
+            <br style="clear: left" />
+        </div> <!-- end of templatemo_menu -->
+        <div class="cleaner"></div>
+    </div> <!-- end of templatemo header -->
+    
+    <div id="templatemo_middle">
+    	<div id="mid_slider">
+			<h3 class="verticalar">Slider Goes Here</h3>
+        </div>
+        <div id="mid_left">
+            <div id="mid_title">
+                محتوي عرعربي محتو عنوان
+            </div>
+            <p>محتوي عربي محتوي عربي محتوي عربي محتوي عربي  محتوي عربي محتوي عربي محتوي عربي محتوي عربي محتوي عربي محتوي عربي محتويمحتوي عربي .</p>
+            <div id="learn_more"><a href="#">اعرف اكثر</a></div>
+	  	</div>
+        <div class="cleaner"></div>
+	</div> <!-- end of middle -->
